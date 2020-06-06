3241d6cdb45ba4c786346f34c3c31b78399c7253
kuojed <kuojed@gmail.com>
2020-06-06 20:32:52 +0800

    Initial Commit

---
+++ pointCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pointCard
+//
+//  Created by 郭冠杰 on 2020/6/6.
+//  Copyright © 2020 郭冠杰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
